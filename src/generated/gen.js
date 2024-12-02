b271cca2c7573c0fc46673e194b665c634b80d44
Tiagorios03 <riossanti307@gmail.com>
2024-12-02 14:41:30 -0500

    Add files via upload

---
+++ src/generated/gen.js
@@ -0,0 +1,16 @@
+﻿globalThis["needle:dependencies:ready"] = import("./register_types.ts")
+
+export const needle_exported_files = new Array();
+globalThis["needle:codegen_files"] = needle_exported_files;
+needle_exported_files.push("assets/SampleScene.glb?v=1733117612217");
+document.addEventListener("DOMContentLoaded", () =>
+{
+	const needleEngine = document.querySelector("needle-engine");
+	if(needleEngine && needleEngine.getAttribute("src") === null)
+	{
+		needleEngine.setAttribute("hash", "1733117612217");
+		needleEngine.setAttribute("src", JSON.stringify(needle_exported_files));
+	}
+});
+
+console.log("Made\ with\ ♥\ by\ 🌵\ Needle\ -\ https://needle\.tools\ —\ Version\ 3\.51\.0");
